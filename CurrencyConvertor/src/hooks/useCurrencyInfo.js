f0725c6b30d15a6f024afc419a569cfa0fa91d57
sidhantrajput1 <sidhant808310@gmail.com>
2025-01-18 16:38:57 +0530

    Currency Convertor

--- CurrencyConvertor/src/hooks/useCurrencyInfo.js
+++ CurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -3,16 +3,12 @@ import { useEffect, useState } from "react";
 function useCurrencyInfo(currency) {
   const [data, setData] = useState({});
   useEffect(() => {
-    fetch(
-      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
-    )
+    fetch(`https://api.frankfurter.app/latest?from=${currency}`)
       .then((res) => res.json())
-      .then((res) => setData(res[currency]));
-    console.log(data);
-  }, [currency, data]);
-  console.log(data)
+      .then((res) => setData(res.rates)); // rates object holds currency rates
+  }, [currency]); // Remove 'data' from the dependency array
+
   return data;
 }
-
 
 export default useCurrencyInfo;
